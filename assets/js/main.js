353fef9c7ff9bf444de727102cff005c46f128d6
snerva <selenenerva@live.it>
2022-10-19 15:13:51 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Visualizzare in pagina 5 numeri casuali
+//Da lì parte un timer di 30 secondi. 
+//Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt(). 
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//creare elemento in dom per randomNumbers
+//generare 5 randomNumbers con Math.random e racchiusi in function e stampa in pagina
+//setTimeout di 30000 che toglie i randomNumbers e fa vedere i prompt()
+//salvare userNumbers inseriti
+//if userNumbers == randomNumbers totalNumbers++ e stampa a video userNumbers e totale
+
